3f5057aaa4e6d45519868cdea80b473a4ae22424
Hvglaser <mail@hannibalglaser.dk>
2026-09-04 13:25:17 +0200

    text file in multilangtextbox

--- Runtime/Utilities/MultiLangTextBox.cs
+++ Runtime/Utilities/MultiLangTextBox.cs
@@ -1,41 +1,51 @@
-
 using UnityEngine;
 
 namespace Muco
 {
-    [RequireComponent(typeof(TMPro.TextMeshPro))]
-    public class MultiLangTextBox : MonoBehaviour
-    {
+[RequireComponent(typeof(TMPro.TextMeshPro))]
+public class MultiLangTextBox : MonoBehaviour
+{
 
-        [TextArea]
-        public string Dansk;
-        [TextArea]
-        public string English;
+[Header("Manual input")]
+[TextArea]
+public string Dansk;
+[TextArea]
+public string English;
 
-        [TextArea]
-        public string Deutsch;
-        // Might want to do this dynamically based on Language
+[TextArea]
+public string Deutsch;
 
-        public void SelectLanguage(Language language)
-        {
-            var textComponent = GetComponent<TMPro.TextMeshPro>();
+[Header("Text file override (takes precedence over manual input)")]
+public TextAsset danskFile;
+public TextAsset englishFile;
+public TextAsset deutschFile;
+// Might want to do this dynamically based on Language
 
-            switch (language)
-            {
-                case Language.English:
-                    textComponent.text = English.Replace('\n', '\u000a').Replace('\t', '\t');
-                    break;
-                case Language.Dansk:
-                    textComponent.text = Dansk.Replace('\n', '\u000a').Replace('\t', '\t');
-                    break;
-                case Language.Deutsch:
-                    textComponent.text = Deutsch.Replace('\n', '\u000a').Replace('\t', '\t');
-                    break;
-                default:
-                    Debug.Log("Unsupported language: " + language);
-                    break;
-            }
-        }
-        
-    }
+public void SelectLanguage(Language language)
+{
+var textComponent = GetComponent<TMPro.TextMeshPro>();
+
+switch (language)
+{
+case Language.English:
+textComponent.text = Resolve(englishFile, English);
+break;
+case Language.Dansk:
+textComponent.text = Resolve(danskFile, Dansk);
+break;
+case Language.Deutsch:
+textComponent.text = Resolve(deutschFile, Deutsch);
+break;
+default:
+Debug.Log("Unsupported language: " + language);
+break;
+}
+}
+
+private static string Resolve(TextAsset file, string manualText)
+{
+return file != null ? file.text : manualText;
+}
+
+}
 }
